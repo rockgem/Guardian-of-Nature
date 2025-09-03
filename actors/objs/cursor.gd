fb9a178efd795262bd869f2e76371296441ecd82
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2025-09-03 21:01:55 +0800

    add cursor, changed resolution

---
+++ actors/objs/cursor.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
+
+func _process(delta: float) -> void:
+	global_position = get_global_mouse_position()
